6041c281c418cc3ad2fe6ed293019545957e5902
Utkarshbajpai07 <utkarshb928@gmail.com>
2025-07-08 23:46:41 +0530

    day_1

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail="utkarshb928@gmail.com"
+var accountPassword = "123456"
+accountCity="Udaipur"
+let accountState
+
+// accountId=2 // not allowed
+accountEmail = "abcd@xyz.com"
+accountPassword ="qwert"
+accountCity ="Uttar Pradesh"
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState])
+
